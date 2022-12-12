c85a1b0ae98dc006cb62403f6792dbef6b2002b1
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-12 04:44:01 +0100

    feat: using match expressions

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 
 fn main() {
     //   let foo:bool = false;
-    let total = add(10, 21);
+    let mut total = add(20, 10);
+    let mut free_shipping = false;
 
     if total > 50 {
         println!("You quality for free shipping!");
+        free_shipping = true;
     } else if total > 20 {
         println!("If you add more items, you can qualify for free shipping.")
     } else {
         println!("No free shipping.")
     }
 
-    println!("{:?}", total);
+    match free_shipping {
+        true => total = total + 0,
+        false => total = total + 5,
+    }
+
+    println!("Total: {:?}", total);
 }
