5e5a67bc75b73b15af15835333fbcd7921e69660
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-12 04:10:30 +0100

    feat: using macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn add(num_one: i32, num_two: i32) -> i32 {
+    num_one + num_two
 }
+
+fn main() {
+//    let foo:bool = false;
+   let foo = add(10, 45);
+    println!("{}", foo); // 55
+    println!("{} {}", foo, true); // 55 true
+    println!("{0} {0}", foo); // 55 55
+    println!("{:?}", foo); // 55
+}   
